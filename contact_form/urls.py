5a8eb0774281f79db022533e03d8b89509bcda8a
Mike Fogel <mike@fogel.ca>
2011-03-23 23:33:59 -0700

    using django 1.3 generic class-based views
    I love how this makes the views almost trivial.

--- contact_form/urls.py
+++ contact_form/urls.py
@@ -1,29 +1,14 @@
-"""
-Example URLConf for a contact form.
-
-Because the ``contact_form`` view takes configurable arguments, it's
-recommended that you manually place it somewhere in your URL
-configuration with the arguments you want. If you just prefer the
-default, however, you can hang this URLConf somewhere in your URL
-hierarchy (for best results with the defaults, include it under
-``/contact/``).
-
-"""
-
-
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
 
-from .views import contact_form
+from .views import ContactFormView, ContactFormSentView
 
 urlpatterns = patterns('',
     url(r'^$',
-        view=contact_form,
+        view=ContactFormView.as_view(),
         name='contact-form'
     ),
     url(r'^sent/$',
-        view=direct_to_template,
-        kwargs={'template': 'contact_form/iframes/contact_form_sent.html'},
+        view=ContactFormSentView.as_view(),
         name='contact-form-sent'
     ),
 )
